f4dd7ebfb6ce3fc7891c0edaa02cf7c38402422f
Chris Tartamella <ctartamella@anexinet.com>
2018-05-26 22:31:04 -0400

    consolidate into a single package.

--- flutter_refit/lib/flutter_refit.dart
+++
@@ -1,7 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library flutter_refit;
-
-export 'refit/refit.dart';
-export 'refit/utils.dart';
--- lib/flutter_refit.dart
+++ lib/flutter_refit.dart
@@ -1,8 +1,12 @@
-library flutter_refit_cli;
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_refit;
+
+export 'refit/refit.dart';
 
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
-
-import 'refit_cli/generator.dart';
+import 'build_runner/generator.dart';
 
 Builder refitHttpPartBuilder(BuilderOptions options) => new PartBuilder([new JaguarHttpGenerator()], generatedExtension: ".api.dart");
