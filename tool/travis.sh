fbc5ccaf0ba6d6209c2aab13cd18e45664596178
Chris Tartamella <chris@tartamella.me>
2018-07-06 23:15:26 -0400

    Build.

--- tool/travis.sh
+++ tool/travis.sh
@@ -19,13 +19,8 @@ EXIT_CODE=0
 while (( "$#" )); do
   TASK=$1
   case $TASK in
-  command_0) echo
-    echo -e '\033[1mTASK: command_0\033[22m'
-    echo -e 'pub publish -f'
-    pub publish -f || EXIT_CODE=$?
-    ;;
-  command_1) echo
-    echo -e '\033[1mTASK: command_1\033[22m'
+  command) echo
+    echo -e '\033[1mTASK: command\033[22m'
     echo -e 'pub run build_runner build --delete-conflicting-outputs --fail-on-severe'
     pub run build_runner build --delete-conflicting-outputs --fail-on-severe || EXIT_CODE=$?
     ;;
@@ -38,6 +33,11 @@ while (( "$#" )); do
     echo -e '\033[1mTASK: dartfmt\033[22m'
     echo -e 'dartfmt -n --set-exit-if-changed .'
     dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
+    ;;
+  test) echo
+    echo -e '\033[1mTASK: test\033[22m'
+    echo -e 'pub run test'
+    pub run test || EXIT_CODE=$?
     ;;
   *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
     EXIT_CODE=1
