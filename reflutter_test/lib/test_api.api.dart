370e2b4017ef173affcacd98e78e0df66540df70
Chris Tartamella <ctartamella@anexinet.com>
2018-12-01 19:28:26 -0500

    Add support for removing null query params.

--- reflutter_test/lib/test_api.api.dart
+++ reflutter_test/lib/test_api.api.dart
@@ -25,4 +25,29 @@ class TestApi extends ReflutterApiDefinition implements TestApiDefinition {
         rawResponse);
     return await interceptResponse(response);
   }
+
+  @override
+  Future<ReflutterResponse<UserResponse>> listUsers(
+      {dynamic sortOrder, dynamic sortField}) async {
+    final queryStr = paramsToQueryUri({
+      'sort_order': '$sortOrder',
+      'sort_field': '$sortField',
+    });
+    final url = '$baseUrl/users?$queryStr';
+    var request = ReflutterRequest(
+        method: 'GET',
+        url: url,
+        headers: headers,
+        body: json.encode(sortField));
+    request = await interceptRequest(request);
+    final rawResponse = await request.send(client);
+    if (!ReflutterApiDefinition.responseSuccessful(rawResponse)) {
+      return ReflutterResponse(null, rawResponse);
+    }
+    final response = ReflutterResponse(
+        UserResponse.fromJson(
+            json.decode(rawResponse.body) as Map<String, dynamic>),
+        rawResponse);
+    return await interceptResponse(response);
+  }
 }
--- reflutter_test/lib/test_api.g.dart
+++ reflutter_test/lib/test_api.g.dart
@@ -12,3 +12,11 @@ HealthResponse _$HealthResponseFromJson(Map<String, dynamic> json) {
 
 Map<String, dynamic> _$HealthResponseToJson(HealthResponse instance) =>
     <String, dynamic>{'value': instance.value};
+
+UserResponse _$UserResponseFromJson(Map<String, dynamic> json) {
+  return UserResponse(
+      users: (json['users'] as List)?.map((e) => e as String)?.toList());
+}
+
+Map<String, dynamic> _$UserResponseToJson(UserResponse instance) =>
+    <String, dynamic>{'users': instance.users};
